7f134ad9b1a4f5cec8604ff4463a0319f7e92dcd
Valentin Burillier <valentin.burillier@gmail.com>
2024-02-16 08:58:27 +0100

    test 1 debug new function

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -4,25 +4,25 @@ function changePays() {
   const getUrl = window.location.pathname;
     if(img.lang === 'en') {
       switch(getUrl) {
-        case "/index.html":
-          document.location.href = "/pages/en/home.html";
+        case "/sebayt/index.html":
+          document.location.href = "/sebayt/pages/en/home.html";
           break;
-        case "/pages/navigation/evenements.html":
-          document.location.href = "/pages/en/events.html";
+        case "/sebayt/pages/navigation/evenements.html":
+          document.location.href = "/sebayt/pages/en/events.html";
           break;
-        case "/pages/navigation/espace-adherents.html":
-          document.location.href = "/pages/en/member-area.html";
+        case "/sebayt/pages/navigation/espace-adherents.html":
+          document.location.href = "/sebayt/pages/en/member-area.html";
           break;
       }
     } else {
       if(img.lang === 'fr'){
         switch(getUrl) {
-          case "/pages/en/home.html":
-            document.location.href = '/';
+          case "/sebayt/pages/en/home.html":
+            document.location.href = '/sebayt/';
             break;
-          case "/pages/en/events.html":
+          case "/sebayt/pages/en/events.html":
             console.log('bravo');
-            document.location.href = "/pages/navigation/evenements.html";
+            document.location.href = "/sebayt/pages/navigation/evenements.html";
             break;
         }
       }
